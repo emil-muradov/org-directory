e89376541dc71c47da4e516751ab44a06d95d2eb
Emil Muradov <emil@inkitt.com>
2026-01-22 00:55:58 +0300

    fix di container

--- src/infrastructure/di/container.py
+++ src/infrastructure/di/container.py
@@ -1,30 +1,21 @@
 from dependency_injector import containers, providers
-from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from core.services import OrganizationService
 from infrastructure.persistence.db.repositories import OrganizationRepositoryImpl
 from config.settings import settings
 
 
-def create_db_session(engine: AsyncEngine):
-    session_factory = async_sessionmaker(engine, expire_on_commit=False)
-    return session_factory()
-
 
 class Container(containers.DeclarativeContainer):
-    db_engine = providers.Singleton(
-        create_async_engine,
+    db_engine = create_async_engine(
         settings.db_url.get_secret_value(),
         pool_size=settings.db_pool_size,
         max_overflow=0,
         pool_timeout=30,
     )
-    
-    db_session = providers.Factory(
-        create_db_session,
-        engine=db_engine,
-    )
-    
+    async_session_factory = async_sessionmaker(db_engine, expire_on_commit=False)
+    db_session = providers.Factory(async_session_factory)
     organization_repository = providers.Factory(
         OrganizationRepositoryImpl,
         session=db_session,
