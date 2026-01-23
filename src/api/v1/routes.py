01f58d859685739146fb6f24a5060f8bb1ef4289
Emil Muradov <emil@inkitt.com>
2026-01-23 12:13:39 +0300

    raise exception instead of successful response

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Query, Depends
+from fastapi import APIRouter, HTTPException, Query, Depends
 from fastapi.responses import JSONResponse
 from dependency_injector.wiring import Provide, inject
 
@@ -23,7 +23,7 @@ async def get_organization(
 ) -> OrganizationDTO:
     org = await organization_service.find_organization_by_id(id)
     if org is None:
-        return JSONResponse(status_code=404)
+        raise HTTPException(status_code=404, detail="Organization not found")
     return JSONResponse(content=org.model_dump(), status_code=200)
 
 
